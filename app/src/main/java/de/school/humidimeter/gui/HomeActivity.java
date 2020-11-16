5950c1e240b3dbc4ea5d1248cd887e30cd8c758c
pbrunner <p.brunner@varengold.de>
2020-11-16 12:46:43 +0100

    init App

--- app/src/main/java/de/school/humidimeter/gui/HomeActivity.java
+++ app/src/main/java/de/school/humidimeter/gui/HomeActivity.java
@@ -12,6 +12,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.View;
 
@@ -27,7 +30,30 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        initializeCounter();
+        startJobScheduler();
+
+
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.)
+
+
+        //TODO: Request auf REST-Schnittstelle, um die Luftfeuchtigkeit zu erhalten
+        int humidity = 70;
+        if (humidity < 60) {
+
+        } else if (humidity >= 60 && humidity <= 80) {
+
+        } else if (humidity > 80) {
+
+        }
+
+
+
+
+//        Bundle b = getIntent().getExtras();
+//        if (b.getString(""))
 
 
 
@@ -39,7 +65,7 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    private void initializeCounter() {
+    private void startJobScheduler() {
 //        JobScheduler
 
     }
---
+++ app/src/main/java/de/school/humidimeter/logic/ScheduledJobService.java
@@ -0,0 +1,72 @@
+package de.school.humidimeter.logic;
+
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import de.school.humidimeter.gui.HomeActivity;
+import de.school.humidimeter.gui.HomeDryFragment;
+import de.school.humidimeter.gui.HomeNormalFragment;
+import de.school.humidimeter.gui.HomeWetFragment;
+
+public class ScheduledJobService extends JobService {
+
+    private static final String TAG = "ScheduledJobService";
+    private boolean jobCancelled = false;
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.d(TAG, "Job started");
+        doBackgroundWork(params);
+        return true;
+    }
+    private void doBackgroundWork(final JobParameters params) {
+        new Thread(() -> {
+            //TODO: Laden der Daten aus der REST-Schnittstelle
+            int recommendedVentilationTime = 15;
+
+            Bundle b = new Bundle();
+
+
+
+            switch (recommendedVentilationTime) {
+                case 5:
+                    //TODO: Schicke Push mit 3-5 Minuten Lüftungsempfehlung
+                    b.putString("humidity", "dry");
+                    b.putInt("time", recommendedVentilationTime);
+                    break;
+                case 10:
+                    //TODO: Schicke Push mit 5-10 Minuten Lüftungsempfehlung
+                    b.putString("humidity", "normal");
+                    b.putInt("time", recommendedVentilationTime);
+                    break;
+                case 15:
+                    //TODO: Schicke Push mit 15-20 Minuten Lüftungsempfehlung
+                    b.putString("humidity", "wet");
+                    b.putInt("time", recommendedVentilationTime);
+                    break;
+                default:
+                    return;
+            }
+
+            startActivity(new Intent (this, HomeActivity.class), b);
+
+
+            Log.d(TAG, "Job finished");
+            jobFinished(params, true);
+        }).start();
+    }
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        Log.d(TAG, "Job cancelled before completion");
+        jobCancelled = true;
+        return true;
+    }
+}
